7972c28795dbe62f72503ce38a6342719aefce36
Dua <Rajinder.dua@mail.nidec.com>
2020-04-27 01:47:04 -0400

    Initial Commit For the Calculator

---
+++ index.js
@@ -0,0 +1,87 @@
+let runningTotal = 0;
+let buffer = "0";
+let prevOperator = null;
+
+const screen = document.querySelector(".screen");
+
+document.querySelector(".calc-buttons").addEventListener("click", function (event) {
+  console.log(console.log(event.target));
+  buttonClick(event.target.innerText);
+});
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  rerender();
+}
+
+function handleNumber(value) {
+  if (buffer === "0") {
+    buffer = value;
+  } else {
+    buffer += value;
+  }
+}
+
+function handleSymbol(value) {
+  switch (value) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      prevOperator = null;
+      break;
+    case "=":
+      if (prevOperator === null) {
+        return;
+      }
+
+      flushOperation(parseInt(buffer));
+      prevOperator = null;
+      buffer = "" + runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    default:
+      handleMath(value);
+      break;
+  }
+}
+
+function handleMath(value) {
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  prevOperator = value;
+
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (prevOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (prevOperator === "-") {
+    runningTotal -= intBuffer;
+  } else if (prevOperator === "×") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
+  }
+}
+
+function rerender() {
+  screen.innerText = buffer;
+}
